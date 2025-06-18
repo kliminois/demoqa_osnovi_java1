3c0c9da882d55e6b1030264c43fa518fb2f26c29
kliminois <klimas.vlad@yandex.ru>
2025-06-18 13:33:51 +0300

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,6 +25,7 @@ public class Main {
         System.out.println("b равно с? " + (b == c));
         System.out.println("b больше или равно с? " + (b >= c) + "\n");
 
+
         // Переполнение и диапазоны
         d = Double.MAX_VALUE;
         System.out.println("Переполнение при арифметической операции");
